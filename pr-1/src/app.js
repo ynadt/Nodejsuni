ea2fde734ba4262335414a349dcf43deb4afd422
ynadt <2769286@gmail.com>
2025-12-07 11:34:58 +0300

    feat: add basic cors

--- pr-1/src/app.js
+++ pr-1/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
 const notFound = require('./middleware/notFound');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
